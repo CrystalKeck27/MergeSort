5b383a7c8a775b9df61547e886e5f72f89cf08aa
crystal <meaneea@gmail.com>
2023-05-25 23:04:16 -0500

    Initial Commit

---
+++ MergeSort.md
@@ -0,0 +1,4 @@
+# Lab 6: Merge Sort
+## Learning Outcomes
+ - Use the compareTo() method from the Comparable interface to determine which of two objects is bigger
+ - Understand and apply recusion in algorithm development
--- src/keckc/MergeSort.java
+++ src/keckc/MergeSort.java
@@ -1,2 +1,56 @@
-package keckc;public class MergeSort {
+package keckc;
+
+import java.util.List;
+
+public class MergeSort {
+    public static <T extends Comparable<T>> void mergeSort(List<T> a) {
+        mergeSort(a, 0, a.size() - 1);
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(List<T> a, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(a, left, mid);
+            mergeSort(a, mid + 1, right);
+
+            merge(a, left, mid, right);
+        }
+    }
+
+    private static <T extends Comparable<T>> void merge(List<T> a, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        T[] temp = (T[]) new Comparable[right - left + 1];
+
+        while (i <= mid && j <= right) {
+            if (a.get(i).compareTo(a.get(j)) < 0) {
+                temp[k] = a.get(i);
+                i++;
+            } else {
+                temp[k] = a.get(j);
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i <= mid) {
+            temp[k] = a.get(i);
+            i++;
+            k++;
+        }
+
+        while (j <= right) {
+            temp[k] = a.get(j);
+            j++;
+            k++;
+        }
+
+        for (k = 0; k < temp.length; k++) {
+            a.set(left + k, temp[k]);
+        }
+    }
 }
